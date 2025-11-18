cf18739b5fd949e51240f9341c4e1a646cdc75e9
Bruno Medeiros <brunojcm@gmail.com>
2025-11-18 23:50:13 +0800

    chore(gradle): convert buildSrc to build-logic

--- buildSrc/src/main/kotlin/conventions.kt
+++
@@ -1,5 +0,0 @@
-import org.gradle.plugin.use.PluginDependenciesSpec
-
-fun PluginDependenciesSpec.standardMultiplatformModule() {
-    id("standard-kotlin-multiplatform")
-}
--- event-store/test-harness-model/build.gradle.kts
+++ event-store/test-harness-model/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    standardMultiplatformModule()
+    id("standard-kotlin-multiplatform")
     kotlin("plugin.serialization")
 }
 
