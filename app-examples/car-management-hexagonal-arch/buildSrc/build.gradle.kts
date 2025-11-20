4b036425b9daae40c4bc37637cad638622b98f54
Bruno Medeiros <brunojcm@gmail.com>
2025-11-20 14:13:09 +0800

    upgrade example app

--- app-examples/car-management-hexagonal-arch/buildSrc/build.gradle.kts
+++ app-examples/car-management-hexagonal-arch/buildSrc/build.gradle.kts
@@ -11,6 +11,6 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    implementation(kotlin("serialization", version = "1.9.22"))
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
+    implementation(kotlin("serialization", version = "2.2.21"))
 }
