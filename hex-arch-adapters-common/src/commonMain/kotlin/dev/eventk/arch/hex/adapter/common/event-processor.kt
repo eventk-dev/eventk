3c2491a4834b44883346314713841d1446967472
Bruno Medeiros <brunojcm@gmail.com>
2026-05-17 17:01:19 +0800

    restore public methods

--- hex-arch-adapters-common/src/commonMain/kotlin/dev/eventk/arch/hex/adapter/common/event-processor.kt
+++ hex-arch-adapters-common/src/commonMain/kotlin/dev/eventk/arch/hex/adapter/common/event-processor.kt
@@ -6,6 +6,7 @@ import dev.eventk.arch.hex.port.SingleStreamTypeEventListener
 import dev.eventk.store.api.EventEnvelope
 import dev.eventk.store.api.StreamType
 import dev.eventk.store.api.blocking.EventStore
+import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.channelFlow
@@ -21,6 +22,31 @@ public fun EventStore.listenerEventFlow(
         is SingleStreamTypeEventListener<*, *> -> { pos -> loadEventBatch(pos, batchSize, eventListener.streamType as StreamType<Any, Any>) }
         is MultiStreamTypeEventListener<*, *> -> { pos -> loadEventBatch(pos, batchSize) }
     }
+    produce(sincePosition, loader, batchSize)
+}
+
+public fun <E, I> EventStore.singleStreamTypeEventFlow(
+    streamType: StreamType<E, I>,
+    sincePosition: Long,
+    batchSize: Int,
+): Flow<EventEnvelope<Any, Any>> = channelFlow {
+    val loader: (Long) -> List<EventEnvelope<Any, Any>> = { pos -> loadEventBatch(pos, batchSize, streamType as StreamType<Any, Any>) }
+    produce(sincePosition, loader, batchSize)
+}
+
+public fun EventStore.multiStreamTypeEventFlow(
+    sincePosition: Long,
+    batchSize: Int,
+): Flow<EventEnvelope<Any, Any>> = channelFlow {
+    val loader: (Long) -> List<EventEnvelope<Any, Any>> = { pos -> loadEventBatch(pos, batchSize) }
+    produce(sincePosition, loader, batchSize)
+}
+
+private suspend fun ProducerScope<EventEnvelope<Any, Any>>.produce(
+    sincePosition: Long,
+    loader: (Long) -> List<EventEnvelope<Any, Any>>,
+    batchSize: Int,
+) {
     var lastPosition = sincePosition
     while (true) {
         val eventBatch = loader(lastPosition)
