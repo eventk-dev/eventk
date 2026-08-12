08c5333a320d70c64ea3f0b60685653907db488b
Edrick Leong <10529706+edrickleong@users.noreply.github.com>
2026-08-12 13:30:02 +0800

    fix: publish the KMP root module alongside the JVM artifacts

--- publish-jvm.sh
+++ publish-jvm.sh
@@ -2,5 +2,6 @@
 
 ./gradlew -Pversion=$1 --parallel --max-workers 8 \
     clean \
+    publishKotlinMultiplatformPublicationToMavenCentralRepository \
     publishJvmPublicationToMavenCentralRepository \
     :hex-arch-adapters-spring6:publish \
